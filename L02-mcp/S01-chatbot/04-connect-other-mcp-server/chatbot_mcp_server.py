ca25e450de37ae71263d5b5dcb33e7a4bc617cb0
pkmer <q10viking@outlook.com>
2026-06-06 13:55:41 +0800

    parse server_config.json success work in progress

---
+++ L02-mcp/S01-chatbot/04-connect-other-mcp-server/chatbot_mcp_server.py
@@ -0,0 +1,117 @@
+"""
+Build a MCP Server exposes two tools
+"""
+
+import json
+import logging
+import time
+
+from pathlib import Path
+
+import arxiv
+from mcp.server.fastmcp import FastMCP
+
+# 配置日志记录到文件
+logging.basicConfig(
+    filename='mcp_server.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+logger = logging.getLogger(__name__)
+
+PAPERS_DIR = "papers"
+
+
+"""Step 01
+Initialize an FastMCP Server
+"""
+
+mcp = FastMCP("research arxiv")
+
+
+"""Step 02
+Decorating the function with @mcp.tool().
+FastMCP automatically generates the necessary MCP Schema base 
+on type hints and doc-strings.
+"""
+
+@mcp.tool()
+def search_pages(topic: str, max_results: int = 5) -> list[str]:
+    """
+    Search for papers on arXiv based on a topic and store their information.
+    Args:
+        topic: The topic to search for
+        max_results: Maximum number of results to retrieve
+    """
+    # Use arxiv to find the papers
+    logger.info(f"Searching for papers on arXiv based on topic: {topic}")
+    client = arxiv.Client()
+
+        # build search request
+    search_req = arxiv.Search(
+        query=topic,
+        max_results= min(max_results,5), 
+        sort_by=arxiv.SortCriterion.Relevance
+    )
+    
+    print(f"查询的论文数量{search_req.max_results}")
+    papers = client.results(search_req)
+
+    # store to json file
+    path = Path(f"../{PAPERS_DIR}") / (topic.replace(" ", "_"))
+    path.mkdir(exist_ok=True, parents=True)
+    file_path = path / "papers_info.json"
+    # load original paper info
+    try:
+        with open(file_path, mode="r", encoding="utf-8") as f:
+            papers_info = json.load(f)
+    except (FileNotFoundError, json.JSONDecodeError):
+        print("init papers_info to empty {}")
+        papers_info = {}
+
+    paper_ids = []
+
+    for paper in papers:
+        paper_id = paper.get_short_id()
+        paper_ids.append(paper_id)
+        paper_info = {
+            'title': paper.title,
+            'authors': [author.name for author in paper.authors],
+            'summary': paper.summary,
+            'pdf_url': paper.pdf_url,
+            'published': str(paper.published.date())
+        }
+        papers_info[paper_id] = paper_info
+
+    with open(file_path, mode="wt", encoding="utf-8") as f:
+        json.dump(papers_info, f, indent=2)
+    logger.info(f"Results save in {file_path.resolve()}")
+    return paper_ids
+
+@mcp.tool()
+def extra_info(paper_id: str) -> str:
+    """
+    Search for information about a specific paper across all topic directories.
+    Args:
+        paper_id: The ID of the paper to look for
+    """
+    logger.info(f"Searching for information about a specific paper: {paper_id}")
+    path = Path(f"../{PAPERS_DIR}")
+
+    for item in path.iterdir():
+        if item.is_dir():
+            target_file = item / "papers_info.json"
+            try:
+                with open(target_file) as f:
+                    papers_info = json.load(f)
+                    if paper_id in papers_info:
+                        return json.dumps(papers_info[paper_id], indent=2)
+            except (FileNotFoundError, json.JSONDecodeError) as e:
+                print(f"Error reading: {target_file.resolve()} \n {e}")
+    return ""
+
+
+# uv run mcp dev chatbot_mcp_server.py
+if __name__ == '__main__':
+    logger.info(f"Starting MCP Server... at {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}")
+    mcp.run(transport='stdio')
---
+++ L02-mcp/S01-chatbot/04-connect-other-mcp-server/mcp_chatbot.py
@@ -0,0 +1,103 @@
+import asyncio
+import json
+import os
+import sys
+
+from mcp import StdioServerParameters, stdio_client, ClientSession
+from openai import OpenAI
+
+
+class McpChatBot:
+    def __init__(self):
+        self.session: ClientSession = None
+        self.available_tools: list[dict] = []
+        self.client = OpenAI(
+            api_key=os.environ.get('DEEPSEEK_API_KEY'),
+            base_url="https://api.deepseek.com")
+
+    async def invoke_llm(self, messages):
+        """
+        Endpoint: /chat/completions
+        Supported in DeepSeek.
+        """
+        response = self.client.chat.completions.create(
+            model="deepseek-v4-flash",
+            messages=messages,
+            tools=self.available_tools,  # bind tools
+            extra_body={"thinking": {"type": "disabled"}}
+        )
+        return response.choices[0].message
+
+    async def process(self, query: str):
+        msgs = [
+            {'role': 'system', 'content': '你的名字叫AxShenZ,由"鲨鱼のJavthon"开发出来的'},
+            {'role': 'user', 'content': query}]
+
+        while True:
+            msg = await self.invoke_llm(msgs)
+            if msg.tool_calls:
+                print(msg.content)
+                # Keep the tool calls info that AI will invoke
+                msgs.append(msg)
+
+                # call the tool
+                for tool in msg.tool_calls:
+                    result = await self.session.call_tool(tool.function.name, json.loads(tool.function.arguments))
+                    msgs.append({"role": "tool", "tool_call_id": tool.id, "content": f"{result}"})
+            elif msg.content:
+                print(msg.content)
+                break
+
+    async def chat_loop(self):
+        print("Input query or 'quit/q' to exit")
+        while (query := input("Query> ").strip().lower()) not in {'quit', 'q'}:
+            await self.process(query)
+            print("\n")
+        else:
+            print("See you next time")
+            sys.exit(0)
+
+    async def connect_to_server_and_run(self):
+        server_params = StdioServerParameters(
+            command='uv',
+            args=['run', 'chatbot_mcp_server.py'],
+            env=None
+        )
+
+        # Launch the server as subprocess & returns the read/write streams
+        # read: the stream that client will use to read msgs from the server
+        # write: the stream that client will use to write msgs to the server
+        async with stdio_client(server_params) as (read, write):
+            # The client session is used to initialize the connection
+            # and send request to the server
+            async with ClientSession(read, write) as session:
+                self.session = session
+                # Initialize the connection(1:1 connection with the server)
+                await session.initialize()
+
+                # list available tools
+                response = await session.list_tools()
+                tools = response.tools
+                print('\n Connected to server with tools: ',
+                      [tool.name for tool in tools])
+                self.available_tools = [{
+                    "type": "function",
+                    "function": {
+                        "name": tool.name,
+                        "description": tool.description,
+                        "parameters": tool.inputSchema
+                    }
+                } for tool in response.tools]
+
+                await self.chat_loop()
+                # import json
+                # print(json.dumps(self.available_tools,indent=2))
+
+
+async def main():
+    chat_bot = McpChatBot()
+    await chat_bot.connect_to_server_and_run()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
